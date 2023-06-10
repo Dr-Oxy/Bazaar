ab1c47a7091f4de46bacb7331e44680a60c721df
droxy <chiomaotu22@gmail.com>
2023-06-10 19:42:41 +0100

    set up config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary-green': '#125252',
+        'primary-gray': '#727272',
+        'primary-red': '#D55015',
+        'off-white': '#f4f4f4',
+        'bazaar-black': '#121212',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-banner':
+          ' linear-gradient(90.19deg, #FFF4EF 47.08%, #fff 140.19%)',
       },
     },
   },
   plugins: [],
-}
+};
