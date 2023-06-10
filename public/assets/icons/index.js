80b7c412bdc92e066a33e21d4cbf2ba5771ff451
droxy <chiomaotu22@gmail.com>
2023-06-10 19:43:45 +0100

    added layout icons

---
+++ public/assets/icons/index.js
@@ -0,0 +1,21 @@
+import clockIconBlack from './clock.svg';
+import messageIconBlack from './message.svg';
+import hamburgerIcon from './hamburge-menu.svg';
+import searchIconwhite from './search.svg';
+import locationIconWhite from './location.svg';
+import dangerIconWhite from './danger-white.svg';
+import heartIconWhite from './heart-white.svg';
+import cartIconWhite from './cart-white.svg';
+import profileAvartar from './profile-avatar.svg';
+
+export {
+  clockIconBlack,
+  messageIconBlack,
+  hamburgerIcon,
+  searchIconwhite,
+  locationIconWhite,
+  dangerIconWhite,
+  heartIconWhite,
+  cartIconWhite,
+  profileAvartar,
+};
