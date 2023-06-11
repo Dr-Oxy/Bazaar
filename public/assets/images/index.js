712735663f6185074bf3a3a48f107cb158cd28ab
droxy <chiomaotu22@gmail.com>
2023-06-11 07:46:07 +0100

    modified dummy data

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -1,3 +1,10 @@
 import bazaarIcon from './logo.png';
+import ads1 from './ad-1.png';
+import ads2 from './ad-2.png';
+import ads3 from './ads-3.png';
+import groceries from './groceries.png';
+import fruits from './fruits.png';
+import rice from './rice.png';
+import tomatoes from './tomatoes.png';
 
-export { bazaarIcon };
+export { bazaarIcon, ads1, ads2, ads3, rice, groceries, fruits, tomatoes };
---
+++ utils/data.js
@@ -0,0 +1,103 @@
+import { tomatoes, fruits, rice, groceries } from '@/public/assets/images';
+
+export const cateogories = [
+  {
+    id: 1,
+    title: 'groceries',
+    image: groceries,
+  },
+  {
+    id: 2,
+    title: 'fruits',
+    image: fruits,
+  },
+  {
+    id: 3,
+    title: 'rice',
+    image: rice,
+  },
+  {
+    id: 4,
+    title: 'fruits',
+    image: fruits,
+  },
+  {
+    id: 5,
+    title: 'rice',
+    image: rice,
+  },
+  {
+    id: 6,
+    title: 'groceries',
+    image: groceries,
+  },
+  {
+    id: 7,
+    title: 'rice',
+    image: rice,
+  },
+  {
+    id: 8,
+    title: 'groceries',
+    image: groceries,
+  },
+  {
+    id: 9,
+    title: 'fruits',
+    image: fruits,
+  },
+  {
+    id: 10,
+    title: 'groceries',
+    image: groceries,
+  },
+  {
+    id: 11,
+    title: 'fruits',
+    image: fruits,
+  },
+  {
+    id: 12,
+    title: 'rice',
+    image: rice,
+  },
+];
+
+export const products = [
+  {
+    id: 1,
+    name: 'Foreign perboiled rice 500g',
+    image: tomatoes,
+    price: 'N2035',
+    ratings: 3.5,
+    isPromo: true,
+    units: 3,
+  },
+  {
+    id: 2,
+    name: 'Foreign perboiled rice 500g',
+    image: fruits,
+    price: 'N2035',
+    ratings: 3.2,
+    isPromo: true,
+    units: 4,
+  },
+  {
+    id: 3,
+    name: 'Foreign perboiled rice 500g',
+    image: rice,
+    price: 'N2035',
+    ratings: 4.1,
+    isPromo: true,
+    units: 2,
+  },
+  {
+    id: 4,
+    name: 'Foreign perboiled rice 500g',
+    image: tomatoes,
+    price: 'N2035',
+    ratings: 3.5,
+    isPromo: true,
+    units: 1,
+  },
+];
