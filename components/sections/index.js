33456b5b4ab8bc9f47f4b10085dedcd38c0f7599
droxy <chiomaotu22@gmail.com>
2023-06-26 15:21:38 +0100

    created testimony section

--- components/sections/index.js
+++ components/sections/index.js
@@ -3,3 +3,5 @@ export { default as ProductCard } from './ProductCard';
 export { default as FlashCard } from './FlashCard';
 export { default as ProductImage } from './ProductImage';
 export { default as RelatedProduct } from './RelatedProduct';
+export { default as Testimonial } from './Testimonial';
+export { default as ReviewCard } from './ReviewCard';
