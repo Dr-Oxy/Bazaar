6e0e7b4a49dd3b9f8dbfd56c357da3539173e1ca
droxy <chiomaotu22@gmail.com>
2023-07-01 20:13:25 +0100

    mobile responisve of special packs item

--- components/sections/SpecialPack.js
+++ components/sections/SpecialPack.js
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 const SpecialPack = ({ offer }) => {
   return (
-    <div className="bg-[#FFF0F0] px-4 w-[90%] flex items-start gap-x-4">
+    <div className="bg-[#FFF0F0] px-2 md:px-4 w-[90%] flex items-start gap-x-2 md:gap-x-4">
       <div>
         <h6 className="font-medium text-sm capitalize">{offer.title}</h6>
         <p className="text-sm text-primary-red">{offer.price}</p>
       </div>
 
-      <div>
+      <div className="flex-1">
         <Image src={offer.image} alt={offer.title} />
       </div>
     </div>
